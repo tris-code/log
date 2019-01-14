70fc4991a057cd05c7aa522c2d0edf6f8fe5792a
Tony Freeman <tonyfreeman@users.noreply.github.com>
2019-01-14 21:41:24 +0000

    [Cleanup] Delete copyright notice

--- Tests/LogTests/LogTests.swift
+++ Tests/LogTests/LogTests.swift
@@ -1,14 +1,3 @@
-/******************************************************************************
- *                                                                            *
- * Tris Foundation disclaims copyright to this source code.                   *
- * In place of a legal notice, here is a blessing:                            *
- *                                                                            *
- *     May you do good and not evil.                                          *
- *     May you find forgiveness for yourself and forgive others.              *
- *     May you share freely, never taking more than you give.                 *
- *                                                                            *
- ******************************************************************************/
-
 import Test
 @testable import Log
 
@@ -60,7 +49,7 @@ class LogTests: TestCase {
 
         Log.error("log")
         assertEqual(log.output, "[error] log")
-        
+
         Log.critical("log")
         assertEqual(log.output, "[critical] log")
     }
